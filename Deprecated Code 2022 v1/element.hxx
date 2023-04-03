603b57c64825b758a6739a222773d86dbd018bed
Unknown-Stryker <122397312+Unknown-Stryker@users.noreply.github.com>
2023-04-03 23:40:41 +0900

    Add files via upload

---
+++ Deprecated Code 2022 v1/element.hxx
@@ -0,0 +1,35 @@
+﻿#pragma once
+#ifndef _ELEMENT_HXX_
+#define _ELEMENT_HXX_
+// UNKNOWN STRYKER and Hojin Joey Lee are the same person
+// Copyright © 2022 UNKNOWN STRYKER. All Rights Reserved
+// Copyright © 2022 Hojin Joey Lee. All Rights Reserved
+#pragma warning (disable : 26495)
+#include "common_definitions.h"
+#include "ELEMENT_STATUS.h"
+#include "mstring.hpp"
+
+
+FROGMAN_TEMPLATE_STRUCT_LIBRARY
+{
+	template<typename T>
+	frogman_fundamental_struct element
+	{
+		T m_data;
+		FFC::mstring m_tag;
+		FFE::ELEMENT_STATUS m_element_status;
+
+	constructors:
+		element();
+		element(T data_p);
+	};
+
+
+	template<typename T>
+	inline element<T>::element() : m_tag("\0"), m_element_status(FFE::ELEMENT_STATUS::INVALID) {  }
+	
+	template<typename T>
+	inline element<T>::element(T data_p) : m_data(data_p), m_tag("\0"), m_element_status(FFE::ELEMENT_STATUS::VALID) {  }
+}
+
+#endif
---
+++ Deprecated Code 2022 v1/typeinfo.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#ifndef _TYPEINFO_H_
+#define _TYPEINFO_H_
+// UNKNOWN STRYKER and Hojin Joey Lee are the same person
+// Copyright © 2022 UNKNOWN STRYKER. All Rights Reserved
+// Copyright © 2022 Hojin Joey Lee. All Rights Reserved
+#include "common_definitions.h"
+#include "TYPE_COMPARISON_RESULT.h"
+#include "mstring.hpp"
+using FFC::mstring;
+
+
+FROGMAN_FUNDAMENTAL_FUNCTION_LIBRARY
+{
+	bool Is_it__ptr(auto pointer);
+
+	FFE::TYPE_COMPARISON_RESULT compare_data_types(auto typename_A_p, auto typename_B_p);
+
+	mstring GET_type_name(auto data);
+
+	hash GET_type_hash(auto data);
+}
+#endif
